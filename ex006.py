56c425a2d304d8c429b1b9de9b1d3f8244b990b1
Yarlley Fernanes <147113204+Yarlley31@users.noreply.github.com>
2023-11-02 09:23:44 -0300

    desafio 006

---
+++ ex006.py
@@ -0,0 +1,8 @@
+n1 = int(input('Digite um número: '))
+dobro = n1 * 2
+triplo = n1 * 3
+rzq = n1**(1/2)
+
+print(f'O dobro do valor inserido é {dobro}')
+print(f'O triplo do valor inserido é {triplo}')
+print(f'A raiz quadrada do valor inserido é {rzq}')
